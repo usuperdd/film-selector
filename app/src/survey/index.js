ce40a838e0aab1b0ad8f088b31110e8a6c353aae
ellie <ellie@kmong.com>
2021-11-28 17:58:30 +0900

    survey

--- app/src/survey/index.js
+++ app/src/survey/index.js
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import {Row, Col} from "react-bootstrap";
+import { Row, Col } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 import ActionImg from "../images/action.jpeg";
@@ -8,78 +8,77 @@ import FantasyImg from "../images/fantasy.jpeg";
 import ScienceFiction from "../images/sf.jpeg";
 
 const Wrapper = styled.div`
-/* height: 100vh;   */
-background-color: #001F3F;
-color: white;
-padding: 20px;
-
+  /* height: 100vh;   */
+  background-color: #001f3f;
+  color: white;
+  padding: 20px;
 `;
 const Heading = styled.h4`
-margin-bottom: 30px;
+  margin-bottom: 30px;
 `;
 
 const Box = styled.div`
-width: 80%;
-text-align: center;
-height: 400px;
-color:white;
-background-color: black;
-display: flex;
-flex-direction: column;
-
+  width: 80%;
+  text-align: center;
+  height: 400px;
+  color: white;
+  background-color: black;
+  display: flex;
+  flex-direction: column;
+  flex: 1;
 `;
 
-const Title  = styled.div`
-height: 15%;
-font-size:30px;
+const Title = styled.div`
+  font-size: 30px;
+  height: 15%;
 `;
 
 const ImageContainer = styled.div`
-display: flex;
+  height: 85%;
 `;
 
 const Image = styled.img`
-width: 100%;
-height: auto;
-/* object-fit: cover; */
+  width: 100%;
+  height: 100%;
 `;
 
-
-
 function Survey() {
-    const styles = {
-        row: {
-            marginBottom: 40
-        }
-    }
-    return (
-        <Wrapper>
-            <Heading>Please select your preferred genre</Heading>
-            <Row style={styles.row}>
-                <Col>
-                    <Box>
-                        <Title>Action </Title>
-                        <ImageContainer>dd</ImageContainer>
-                        {/* <Image src={ActionImg}/> */}
-                        
-                    {/* <Image src={ActionImg}/>  */}
-                      
-                    </Box>
-                </Col>
-                <Col>
-                    <Box>
-                        {/* Drama
+  const styles = {
+    row: {
+      marginBottom: 40,
+    },
+  };
+  return (
+    <Wrapper>
+      <Heading>Please select your preferred genre</Heading>
+      <Row style={styles.row}>
+        <Col>
+          <Box>
+            <Title>Action</Title>
+            <ImageContainer>
+              <Image src={ActionImg} />
+            </ImageContainer>
+            {/* <Image src={ActionImg} /> */}
+            {/* <ImageContainer><Image src={ActionImg} /></ImageContainer> */}
+            {/* <Image src={ActionImg}/> */}
+
+            {/* <Image src={ActionImg}/>  */}
+          </Box>
+        </Col>
+        <Col>
+          <Box>
+            {/* Drama
                         <Image src={DramaImg}/> */}
-                    </Box>
-                </Col>
-                <Col>
-                    <Box>
-                        {/* Fantasy
+          </Box>
+        </Col>
+        <Col>
+          <Box>
+            {/* Fantasy
                         <Image src={FantasyImg}/> */}
-                    </Box>
-                </Col>
-            </Row>
-            {/* <Row style={styles.row}>
+          </Box>
+        </Col>
+      </Row>
+      {/* <Row style={styles.row}>
                 <Col>
                     <Box>
                         SF
@@ -92,9 +91,8 @@ function Survey() {
                 </Col>
             </Row>
            */}
-          
-        </Wrapper>
-    )
+    </Wrapper>
+  );
 }
 
 export default Survey;
